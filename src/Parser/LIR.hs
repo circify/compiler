8d7dbd80db62e5cc4b82c742f5b87fdf77313521
mlfbrown <mlfbrown@stanford.edu>
2020-02-25 15:28:56 -0800

    parse in the block json data so far

--- src/Parser/LIR.hs
+++ src/Parser/LIR.hs
@@ -1,23 +1,31 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Parser.LIR where
 import           Data.Aeson
+import           Data.Text
 import           GHC.Generics
 
 data Block = Block { blockNumber  :: Int
-                   , instructions :: [String]
+                   , instructions :: [Text]
                    } deriving (Generic, Show)
 
-data AST = AST [Block] deriving (Generic, Show)
+data Comparison = Comparison {
+          beforeRegisterAllocation :: [Block]
+        , afterRegisterAllocation  :: [Block]
+        } deriving (Generic, Show)
 
-instance ToJSON AST where
+data AllComparisons = AllComparisons [Comparison] deriving (Generic, Show)
+
 instance ToJSON Block where
-instance FromJSON AST where
 instance FromJSON Block where
+instance ToJSON Comparison where
+instance FromJSON Comparison where
+instance ToJSON AllComparisons where
+instance FromJSON AllComparisons where
 
-parseAST :: FilePath -> IO (Maybe AST)
+parseAST :: FilePath -> IO (Maybe AllComparisons)
 parseAST name = decodeFileStrict name
 
 printAST :: IO ()
 printAST = do
-  r <- parseAST "examples/data.json"
+  r <- parseAST "examples/test.json"
   print r
