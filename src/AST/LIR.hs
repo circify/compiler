aa49b3944f5b9db310c52ebcdc334eb747c0bb02
mlfbrown <mlfbrown@stanford.edu>
2020-02-24 22:21:14 -0800

    more definitions before parsing

--- src/AST/LIR.hs
+++ src/AST/LIR.hs
@@ -63,6 +63,7 @@ data LAlloc = LAlloc
 
 data Address = Address
 
+data MDefintion = MDefinition
 data LDefinition = LDefinition
 data LAllocation = LAllocation
 
@@ -70,44 +71,33 @@ data LAllocation = LAllocation
 
 
 -- | Memory operations in LIR
+--
+-- Missing LoadElementFromStateV, which is insane:
+-- https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11746
+--
 data Mem = StoreFixedSlotV { object       :: LAllocation
                            , value        :: LAllocation
                            , slot         :: Slot
                            , needsBarrier :: Bool
                            }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#10951
-         -- Object: ToRegister(ins->getOperand(0)) where getOperand returns an LAllocation
-         -- Value: const ValueOperand value = ToValue(ins, LStoreFixedSlotV::Value)
-         -- Slot: size_t slot = ins->mir()->slot()
-         -- NeedsBarrier: if (ins->mir()->needsBarrier())
          | LoadFixedSlotV { object :: LAllocation
                           , slot   :: Slot
                           , result :: LAllocation
                           }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#10900
-         -- Object: ToRegister(ins->getOperand(0)), see rationale above
-         -- Slot: size_t slot = ins->mir()->slot()
-         -- Result: ValueOperand result = ToOutValue(ins)
          | StoreFixedSlotT { object       :: LAllocation
                            , value        :: LAllocation
                            , slot         :: Slot
                            , needsBarrier :: Bool
                            }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#10965
-         -- Object: const Register obj = ToRegister(ins->getOperand(0))
-         -- ALLOC VALUE UNCLEAR
-         -- Slot: size_t slot = ins->mir()->slot()
-         -- NeedsBarrier: if (ins->mir()->needsBarrier())
          | LoadFixedSlotT { object  :: LAllocation
                           , slot    :: Slot
                           , result' :: LDefinition
                           , ty      :: Ty
                           }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#10908
-         -- Object: const Register obj = ToRegister(ins->getOperand(0))
-         -- Slot: size_t slot = ins->mir()->slot()
-         -- Result: https://searchfox.org/mozilla-central/source/js/src/jit/LIR.h#955
-         -- Ty: MIRType type = ins->mir()->type()
          | LoadFixedSlotAndUnbox { input    :: LAllocation
                                  , slot     :: Slot
                                  , result'  :: LDefinition
@@ -115,33 +105,17 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                                  , fallible :: Bool
                                  }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#10918
-         -- Input: const Register input = ToRegister(ins->getOperand(0)
-         -- Slot: slot = mir->slot()
-         -- Result: AnyRegister result = ToAnyRegister(ins->output()) where:
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/LIR.h#986
-         -- Type: MIRType type = mir->type()
-         -- Falliable: if (mir->fallible())
          | StoreSlotV { base         :: LAllocation
                       , slot         :: Slot
                       , value'       :: (LAllocation, LAllocation)
                       , needsBarrier :: Bool
                       }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#4020
-         -- Base: Register base = ToRegister(lir->slots())
-         -- Slot: lir->mir()->slot()
-         -- Value: const ValueOperand value = ToValue(lir, LStoreSlotV::Value) where
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/arm/CodeGenerator-arm.cpp#1317
-         -- NeedsBarrier: if (lir->mir()->needsBarrier())
          | LoadSlotV { dest' :: (LAllocation, LAllocation)
                      , base  :: LAllocation
                      , slot  :: Slot
                      }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#3985
-         -- Dest: ValueOperand dest = ToOutValue(lir)
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/shared/CodeGenerator-shared-inl.h#162
-         -- Base: Register base = ToRegister(lir->input()) where
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/LIR.h#982
-         -- Slot: lir->mir()->slot()
          | StoreSlotT { base         :: LAllocation
                       , avalue       :: LAllocation
                       , slot         :: Slot
@@ -149,22 +123,12 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                       , needsBarrier :: Bool
                       }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#3993
-         -- Base: Register base = ToRegister(lir->slots()) where lir's slots is LAllocation
-         -- Value: lir->value()
-         -- Slot: lir->mir()->slot()
-         -- Ty: MIRType valueType = lir->mir()->value()->type()
-         -- NeedsBarrier: (lir->mir()->needsBarrier())
          | LoadSlotT { base    :: LAllocation
                      , slot    :: Slot
                      , result' :: LDefinition
                      , ty      :: Ty
                      }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#3977
-         -- Base: Register base = ToRegister(lir->slots())
-         -- Slot: lir->mir()->slot()
-         -- Output: AnyRegister result = ToAnyRegister(lir->output()) where
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/shared/LIR-shared.h#4975
-         -- Type: lir->mir()->type()
          | StoreElementV { value          :: LAllocation
                          , elements       :: LAllocation
                          , index          :: LAllocation
@@ -173,15 +137,6 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                          , needsHoleCheck :: Bool
                          }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9505
-         -- Value: const ValueOperand value = ToValue(lir, LStoreElementV::Value)
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/x64/CodeGenerator-x64.cpp#27
-         -- Elements: Register elements = ToRegister(lir->elements())
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/shared/LIR-shared.h#4076
-         -- Index: const LAllocation* index = lir->index()
-         -- OffsetAdjustement: lir->mir()->offsetAdjustment() where
-         --https://searchfox.org/mozilla-central/source/js/src/jit/MIR.h#7676
-         -- NeedsBarrier: lir->mir()->needsBarrier()
-         -- NeedsHoleCheck: lir->mir()->needsHoleCheck()
          | LoadElementV { elements       :: LAllocation
                         , out'           :: (LAllocation, LAllocation)
                         , index          :: LAllocation
@@ -189,11 +144,6 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                         , needsHoleCheck :: Bool
                         }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11506
-         -- Elements: Register elements = ToRegister(load->elements())
-         -- Out: const ValueOperand out = ToOutValue(load)
-         -- Index: load->index()
-         -- OffsetAdjustment: load->mir()->offsetAdjustment()
-         -- NeedsHolecheck: load->mir()->needsHoleCheck()
          | StoreElementT { value          :: LAllocation
                          , ty             :: Ty
                          , elements       :: LAllocation
@@ -204,14 +154,12 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                          , needsHoleCheck :: Bool
                          }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9487
-         -- Value: store->value()
-         -- Type: store->mir()->value()->type()
-         -- Elements: Register elements = ToRegister(store->elements())
-         -- Element Type: store->mir()->elementType()
-         -- Index: const LAllocation* index = store->index()
-         -- OffsetAdjustment: store->mir()->offsetAdjustment()
-         -- NeedsBarrier: store->mir()->needsBarrier()
-         -- NeedsHoleCheck: store->mir()->needsHoleCheck()
+         | LoadElementT { output'        :: LDefinition
+                        , ty             :: Ty
+                        , loadDoubles    :: Bool
+                        , needsHoleCheck :: Bool
+                        }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11473
          | StoreElementHoleV { elements       :: LAllocation
                              , index          :: LAllocation
                              , value          :: LAllocation
@@ -221,24 +169,62 @@ data Mem = StoreFixedSlotV { object       :: LAllocation
                              , strict         :: Bool
                              }
          -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9576
-         -- Elements: Register elements = ToRegister(lir->elements())
-         -- Index: Register index = ToRegister(lir->index())
-         -- Value: const ValueOperand value = ToValue(lir, T::Value)
-         -- SpectreTemp: ToTempRegisterOrInvalid(lir->spectreTemp()) where
-         -- https://searchfox.org/mozilla-central/source/js/src/jit/shared/LIR-shared.h#4179
-
---         | StoreElementHoleT
-
---          | StoreElementHoleV Value Elements Index SpectreTemp NeedsBarrier
---          | StoreElementHoleT TypedValue Elements Index SpectreTemp NeedsBarrier
---          | LoadElementHole Elements Index InitLength Result NeedsHoleCheck
---          | FallibleStoreElementV Value Elements Index SpectreTemp NeedsBarrier
---          | FallibleStoreElementT TypedValue Elements Index SpectreTemp NeedsBarrier
--- --         | LoadElementFromStateV Index Tmp
---          | StoreUnboxedScalar Value Elements WriteType
---          | LoadUnboxedScalar Elements Index Temp ReadType
---          | StoreTypedArrayElementHole Elements Value ArrayType Index Length
---         | LoadTypedArrayElementHole Object
-
+         | StoreElementHoleT { elements     :: LAllocation
+                             , elementTy    :: Ty
+                             , index        :: LAllocation
+                             , value        :: LAllocation
+                             , ty           :: Ty
+                             , spectreTemp  :: LDefinition
+                             , needsBarrier :: Bool
+                             , strict       :: Bool
+                             }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9531
+         | LoadElementHole { elements              :: LAllocation
+                           , index                 :: LAllocation
+                           , initLength            :: LAllocation
+                           , out'                  :: (LAllocation, LAllocation)
+                           , needsHoleCheck        :: Bool
+                           , needsNegativeIntCheck :: Bool
+                           }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11528
+         | FallibleStoreElementV { elements       :: LAllocation
+                                 , index          :: LAllocation
+                                 , value'         :: (LAllocation, LAllocation) -- ?
+                                 , spectreTemp    :: LDefinition
+                                 , needsHoleCheck :: Bool
+                                 , strict         :: Bool
+                                 }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9576
+         | FallibleStoreElementT { elements       :: LAllocation
+                                 , elementTy      :: Ty
+                                 , index          :: LAllocation
+                                 , value          :: LAllocation
+                                 , ty             :: Ty
+                                 , spectreTemp    :: LDefinition
+                                 , needsBarrier   :: Bool
+                                 , needsHoleCheck :: Bool
+                                 , strict         :: Bool
+                                 }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#9531
+         | StoreUnboxedScalar { elements  :: LAllocation
+                              , value     :: LAllocation
+                              , index     :: LAllocation
+                              , writeTy   :: Ty
+                              , storageTy :: Ty
+                              , offsetAdj :: Int
+                              }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#
+         | LoadUnboxedScalar { elements      :: LAllocation
+                             , index         :: LAllocation
+                             , temp          :: LAllocation
+                             , out           :: LDefinition
+                             , readTy        :: Ty
+                             , cannonDoubles :: Bool
+                             }
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11572
+         | StoreTypedArrayElementHole
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11914
+         | LoadTypedArrayElementHole
+         -- ^ https://searchfox.org/mozilla-central/source/js/src/jit/CodeGenerator.cpp#11602
 
 
