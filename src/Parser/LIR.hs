768265fc0011cd07fd57ab65aa9f928a4090233c
mlfbrown <mlfbrown@stanford.edu>
2020-03-06 17:47:40 -0800

    look at not matching lir node counts

--- src/Parser/LIR.hs
+++ src/Parser/LIR.hs
@@ -2,10 +2,10 @@
 module Parser.LIR where
 import           AST.LIR
 import           AST.Regalloc
-import           Control.Monad (forM_, unless)
+import           Control.Monad (forM_, unless, when)
 import           Data.Aeson
 import qualified Data.Map      as M
-import           Data.Text     hiding (length)
+import           Data.Text     hiding (length, zip)
 
 parseRegAlloc :: FilePath -> IO (Maybe [Graph])
 parseRegAlloc name = decodeFileStrict name
@@ -16,9 +16,22 @@ printRegAlloc = do
   case r of
     Just graphs -> do
       let regs = makeRegallocMap graphs
-          beforeSize = M.size $ beforeRegalloc regs
-          afterSize  = M.size $ afterRegalloc regs
-      unless (beforeSize == afterSize) $
-        error "Different number of blocks before and after regalloc"
-      print regs
+          befores = beforeRegalloc regs
+          afters  = afterRegalloc regs
+          beforeSize = M.size befores
+          afterSize  = M.size afters
+      unless (M.keys befores == M.keys afters) $
+        error "Blocks should have the same numbering before and after regalloc"
+      forM_ (M.keys befores) $ \k -> do
+        let lirBefore = blocks $ befores M.! k
+            lirAfter  = blocks $ afters M.! k
+        unless (length lirBefore == length lirAfter) $
+          error "There should be the same number of blocks before and after regalloc"
+        forM_ (zip lirBefore lirAfter) $ \(beforeBlock, afterBlock) -> do
+          let beforeNodes = nodes beforeBlock
+              afterNodes  = nodes afterBlock
+          when (length beforeNodes /= length afterNodes) $ do
+            putStrLn "---------------------------------------------------"
+            print $ length beforeNodes
+            print $ length afterNodes
     Nothing     -> print "Failed"
