4760d6029386c307d451cc7873f9f58a6809176c
mlfbrown <mlfbrown@stanford.edu>
2020-03-23 11:42:28 -0700

    mir

--- src/Parser/MIR.hs
+++ src/Parser/MIR.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module Parser.LIR where
+module Parser.MIR where
 import           AST.MIR
 import           Control.Monad (foldM, forM_, unless, when)
 import           Data.Aeson
@@ -9,3 +9,5 @@ import qualified Data.Map      as M
 import           Data.Maybe    (fromJust, isJust, isNothing)
 import           Data.Text     hiding (length, null, unwords, zip)
 import           Prelude       hiding (id)
+
+
