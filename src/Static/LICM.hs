68929ba41a6933680535a00dd8b9606cd0e8c05a
mlfbrown <mlfbrown@stanford.edu>
2020-03-24 22:16:48 -0700

    meet and transfer

--- src/Static/LICM.hs
+++ src/Static/LICM.hs
@@ -8,4 +8,16 @@ type SSAName = (Int, Int)
 
 data Deps = Start
           | DepMap { depmap :: M.Map SSAName (S.Set SSAName) }
+          deriving (Eq)
 
+transfer' :: [MIR] -> WorkNode Deps -> IO (WorkNode Deps)
+transfer' = undefined
+
+meet' :: Deps
+      -> Deps
+      -> IO Deps
+meet' = undefined
+
+instance Checkable Deps where
+    meet = meet'
+    transfer = transfer'
