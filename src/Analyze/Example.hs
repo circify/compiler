99c669d262b40c17d600302a8e711a69ef1e243b
mlfbrown <mlfbrown@stanford.edu>
2020-09-01 17:13:36 -0700

    User-defined analysis types that preserve source info

---
+++ src/Analyze/Example.hs
@@ -0,0 +1,35 @@
+module Analyze.Example where
+import           Language.C.Data.Ident
+import           Language.C.Data.Node
+import           Language.C.Syntax.AST
+import           Parser.C
+
+-- | A simple user-defined AST annotation type
+-- You can extend this if it would be helpful
+-- For now it perserves source code location info
+data Decorated a = Decorated { analysisInfo :: a
+                             , locInfo      :: NodeInfo
+                             }
+                 deriving (Eq, Ord, Show)
+
+
+decorate :: a -> NodeInfo -> Decorated a
+decorate = Decorated
+
+-- | Dummy state
+data ExampleState = StartState | EndState
+                  deriving (Eq, Ord, Show)
+
+-- | Uses language-c's built-in (nice) annotation support
+-- See here for more info:
+-- https://hackage.haskell.org/package/language-c-0.8.3/docs/Language-C-Syntax-AST.html#g:9
+initTo :: CTranslUnit -> a -> CTranslationUnit (Decorated a)
+initTo t startState = decorate startState <$> t
+
+-- | Initialize everything to a start state and then print the AST
+analyzeAST :: String -> IO ()
+analyzeAST name = do
+  tu <- parseC name
+  print $ initTo tu StartState
+
+
