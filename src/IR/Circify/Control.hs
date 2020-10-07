80daf6180cb74c9725f07a406dc9159b96e803dc
Lef Ioannidis <elefthei@seas.upenn.edu>
2020-10-07 01:23:52 -0400

    Derive control instances

--- src/IR/Circify/Control.hs
+++ src/IR/Circify/Control.hs
@@ -6,45 +6,54 @@
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveAnyClass #-}
 module IR.Circify.Control where
 
-import Data.Natural
-import Data.String.Interpolate (i)
-import Control.Monad.State.Lazy
+import           Data.Natural
+import           Data.String.Interpolate   (i)
+import           Control.Monad.State.Lazy
 
-newtype Range = Range { start :: Int, end :: Int, step :: Int, iter :: term Int }
+-- Range of a for loop
+data Range = Range { start :: Int, end :: Int, step :: Int }
 
+-- Convenience function that errors on bad ranges
 mkRange :: Int -> Int -> Int -> Range
-mkRange start end step iter
-    | abs (end - start) >= step && step > 0 = Range start end step iter
-    | abs (end - start) >= (- step) && step < 0 = Range start end step iter
+mkRange start end step
+    | abs (end - start) >= step && step > 0 = Range start end step
+    | abs (end - start) >= (- step) && step < 0 = Range start end step
     | otherwise = error [i| Bad loop bounds for(i = #{start}; i < #{end}; i = i + #{step})|]
 
--- Control flow semantics
-data Control (a :: *) (term :: * -> *) where
-    For :: Range -> Control a term -> Control a term
-    While :: term Bool -> Natural -> Control a term -> Control a term
-    If :: term Bool -> Control a term -> Control a term -> Control a term
-    One :: term a -> Control a term
-    Seq :: term b -> Control a term -> Control a term
-    deriving (Functor, Applicative, Monad, Traversable)
+-- Control flow syntax
+data Control (term :: * -> *) (a :: *) where
+    For :: Range -> term Int -> Control term a -> Control term a
+    While :: term Bool -> Natural -> Control term a -> Control term a
+    If :: term Bool -> Control term a -> Control term a -> Control term a
+    One :: term a -> Control term a
+    Seq :: term b -> Control term a -> Control term a
+
+deriving instance Functor term => Functor (Control term)
+deriving instance Applicative term => Applicative (Control term)
+deriving instance Monad term => Monad (Control term)
 
 -- Convenience constructors
-forloop :: Range -> Control a term -> Control a term
-forloop range body = For range body
+forloop :: Range -> term Int -> Control term a -> Control term a
+forloop range iterator body = For range iterator body
 
-whileloop :: term Bool -> Natural -> Control a term -> Control a term
+whileloop :: term Bool -> Natural -> Control term a -> Control term a
 whileloop check maxIterations body = While check maxIterations body
 
-ifstmt :: term Bool -> Control a term -> Control a term -> Control a term
+ifstmt :: term Bool -> Control term a -> Control term a -> Control term a
 ifstmt check left right = If check left right
 
-data LoopAnalysis a term = LoopAnalysis { stack :: Control a term }
+-- Analysis state
+data LoopAnalysis a term = LoopAnalysis { stack :: [Control term a] }
 
-loopAnalysis :: Control a term -> State (LoopAnalysis a term) (Control a term)
+-- TODO: Implement analysis and loop flattening transformation
+loopAnalysis :: Control term a -> State (LoopAnalysis a term) (Control term a)
 loopAnalysis = undefined
 
-loopFlatten :: Control a term -> Control a term
+loopFlatten :: Control term a -> Control term a
 loopFlatten top = undefined
 
 
