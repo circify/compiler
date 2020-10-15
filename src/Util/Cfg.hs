953f9c92c97724335ed5fc087702a01a9b6b7e51
Lef Ioannidis <elefthei@mit.edu>
2020-10-15 15:35:12 -0400

    Add CLI flags for loop flattening and max iterator

--- src/Util/Cfg.hs
+++ src/Util/Cfg.hs
@@ -75,6 +75,8 @@ data CfgState = CfgState { _optR1cs :: Int
                          , _smtOptCfg :: SmtOptCfg
                          , _streams :: [String]
                          , _loopBound :: Int
+                         , _loopFlatten :: Bool
+                         , _loopMaxIteration :: Int
                          , _cCfg :: CCfg
                          , _help :: Bool
                          } deriving (Show)
@@ -85,6 +87,8 @@ defaultCfgState = CfgState { _optR1cs   = 2
                            , _smtOptCfg = defaultSmtOptCfg
                            , _streams   = []
                            , _loopBound = 5
+                           , _loopFlatten = True
+                           , _loopMaxIteration = 10000
                            , _help      = False
                            , _cCfg      = defaultCCfg
                            }
@@ -185,6 +189,16 @@ options =
               "How many iterations loops are unrolled for"
               ""
               "5"
+  , CfgOption (loopFlatten . showReadLens)
+              "loop-flatten"
+              "Enable nested loop flattening optimization"
+              ""
+              "True"
+  , CfgOption (loopMaxIteration . showReadLens)
+              "loop-max-iteration"
+              "Maximum iterator bound for loop flattening optimization"
+              ""
+              "10000"
   , CfgOption (cCfg . printfOutput . showReadLens)
               "c-printf"
               "Handle printf specially"
