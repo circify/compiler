7f6eb38e53de88715bcf682199a79f590b334953
Lef Ioannidis <elefthei@mit.edu>
2020-10-20 20:34:49 -0400

    Added more loop cases, avoid shadowing of new identifiers

--- src/IR/Circify/Control.hs
+++ src/IR/Circify/Control.hs
@@ -2,25 +2,19 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE PostfixOperators #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 module IR.Circify.Control where
 
-import           Control.Monad.Reader (asks)
 import           GHC.Generics
 import           Language.C.Syntax.AST
 import           Language.C.Syntax.Constants
 import           Language.C.Data.Node
 import           Language.C.Data.Position
 import           Language.C.Data.Ident
-import           Util.Cfg
 
 -- Control flow syntax
 data Control term =
@@ -56,103 +50,23 @@ deriving instance Generic term => Generic (Control term)
 class ControlTerm a where
     (<:)  :: a -> a -> a
     (==:) :: a -> a -> a
+    (*:)  :: a -> a -> a
     (=:)  :: a -> Integer -> Control a
     (++:) :: a -> Control a
     lit   :: Integer -> a
     var   :: String -> a
 
+-- Generated code has no debug symbols
 nosym :: NodeInfo
 nosym = OnlyPos nopos (nopos, 0)
 
 instance ControlTerm CExpr where
     a <: b  = CBinary CLeqOp a b nosym
     a ==: b = CBinary CEqOp a b nosym
+    a *: b  = CBinary CMulOp a b nosym
     l =: r  = Term $ CAssign CAssignOp l (lit r) nosym
     (++:) i = Term $ CUnary CPostIncOp i nosym
     lit v   = CConst $ CIntConst (CInteger v DecRepr noFlags) nosym
     var v   = CVar (Ident v 0 nosym) nosym
 
-infixl 8 <:
-infixl 8 ==:
 infixl 7 =:
-
--- Take some program and extract the first loop
--- pullbackLoop (program :: Control term) ->
---    (prologue :: Control term,
---     loop     :: Control term,
---     epilogue :: Control term)
-pullbackLoop :: Control t -> (Control t, Control t, Control t)
-pullbackLoop l@While { .. } = (Empty, l, Empty)
-pullbackLoop l@For   { .. } = (Empty, l, Empty)
-pullbackLoop Seq     { .. } = case (pullbackLoop left, pullbackLoop right) of
-    ((p, l, e), (p', Empty, e'))     -> (p, l, e <> p' <> e')              -- Loop on the left subsequence
-    ((p, Empty, e), (p', l', e'))    -> (p <> e <> p', l', e')             -- Loop on the right subsequence
-    ((p, Empty, e), (p', Empty, e')) -> (p <> e <> p' <> e', Empty, Empty) -- No loop
-pullbackLoop o              = (o, Empty, Empty)
-
--- Main loop flattening transformation
---   Wahby et al. "Efficient RAM and control flow in verifiable outsourced computation"
-loopFlatten' :: ControlTerm t => Integer -> Control t -> Control t
-loopFlatten' maxIteration While { .. } =
-    case pullbackLoop body of
-       (body1, For { body = body2, end = m }, body3) ->
-            state =: 1 <>
-            For dummy (lit 1) (lit maxIteration) (
-                If (state ==: lit 1) (
-                    If (cond) (
-                        body1 <>
-                        i =: 0 <>
-                        state =: 2
-                    ) (state =: 3)
-                ) Empty <>
-                If (state ==: lit 2) (
-                    If (i <: m) (
-                        body2 <>
-                        (i ++:)
-                    ) (state =: 2)
-                ) Empty <>
-                If (state ==: lit 3) (
-                    body3 <>
-                    state =: 1
-                ) Empty
-            )
-       (p, Empty, e) -> p <> e
-       where dummy = var "dummy"
-             state = var "state"
-             i = var "i"
-loopFlatten' maxIteration For { end = n, .. } =
-    case pullbackLoop body of
-        (body1, For { body = body2, end = m }, body3) ->
-            state =: 1 <>
-            i =: 0 <>
-            j =: 0 <>
-            For dummy (lit 1) (lit maxIteration) (
-                If (state ==: lit 1) (
-                    If (i <: n) (
-                        body1 <>
-                        j =: 0 <>
-                        (i ++:)
-                    ) (state =: 2)
-                ) Empty <>
-                If (state ==: lit 2) (
-                    If (j <: m) (
-                        body2 <>
-                        (j ++:)
-                    ) (state =: 3)
-                ) Empty <>
-                If (state ==: lit 3) (
-                    body3 <>
-                    state =: 1
-                ) Empty
-            )
-        (p, Empty, e) -> p <> e
-    where dummy = var "dummy"
-          state = var "state"
-          i = var "i"
-          j = var "j"
-
-loopFlatten :: ControlTerm t => Control t -> Cfg (Control t)
-loopFlatten top = do
-    maxIteration <- asks (Util.Cfg._loopMaxIteration)
-    return $ case pullbackLoop top of
-        (op, loop, ep) -> op <> loopFlatten' maxIteration loop <> ep
