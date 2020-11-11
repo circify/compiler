53e29b9c097d9fd5dfe75930e7d3f2aa557bea6c
Alex Ozdemir <aozdemir@hmc.edu>
2020-11-10 20:46:07 -0800

    LOC: core component

--- eval/loc/count.zsh
+++ eval/loc/count.zsh
@@ -6,6 +6,7 @@ components=(
     circom "../../src/Codegen/Circom"
     z3 "../../src/Targets/SMT/TySmtToZ3.hs"
     r1cs "../../src/IR/R1cs.hs ../../src/IR/R1cs/ ../../src/IR/SMT/ToPf.hs"
+    core "../../src/IR/SMT ../../src/Codegen/Circify.hs ../../src/Codegen/Circify"
     zokrates "../../src/Codegen/Zokrates"
     zokrates-all "../../src/Codegen/Zokrates ../../src/AST/Zokrates.hs ../../src/Parser/Zokrates.hs ../../src/Parser/Zokrates"
 )
