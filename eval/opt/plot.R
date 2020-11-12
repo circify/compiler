c22d06fc5ef667e36cf7834bf4e450e59c5bbe06
Alex Ozdemir <aozdemir@hmc.edu>
2020-11-12 15:43:45 -0800

    Nicer plot

--- eval/opt/plot.R
+++ eval/opt/plot.R
@@ -4,6 +4,7 @@ source("../theme.R")
 
 d <- read_csv("results.csv") %>% mutate(optimized = ifelse(optimized, "Optimized", "Unoptimized"))
 
+print(d)
 
 t <-   t + theme(legend.position = "bottom")
 
@@ -14,10 +15,8 @@ smt_plot <-
   t
 ps_plot <-
   ggplot(d) +
-  geom_point(aes(x = rounds, y = constraints, color = optimized, shape=optimized)) +
-  labs(y = "R1CS Constraints", shape = "Optimization:", color = "Optimization:") +
-  scale_y_continuous(trans="log2") +
-  ylim(0,8000) +
+  geom_point(aes(x = rounds, y = constraints/1000, color = optimized, shape=optimized)) +
+  labs(y = "R1CS Constraints (k)", shape = "Optimization:", color = "Optimization:") +
   t
 ((smt_plot | ps_plot) / guide_area()) + plot_layout(heights = c(5,1),guides = 'collect')
 ggsave("results.png", width = 3, height = 2, units = "in")
